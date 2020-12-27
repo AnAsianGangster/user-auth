e640ea3227b1af2b66e8123f4fc621d0aa8006f2
AnAsianGangster <yangaobo1996@gmail.com>
2020-12-28 00:35:19 +0800

    verify duplication when sign up

--- app/routes/user.route.js
+++ app/routes/user.route.js
@@ -1,7 +1,12 @@
 const controller = require('../controllers/user.controller');
+const verifySignUp = require('../middleware/verifySignUp.middleware');
 
 module.exports = function (router) {
-    router.post('/signup', controller.signup);
+    router.post(
+        '/signup',
+        [verifySignUp.checkDuplicateUsername, verifySignUp.checkDuplicateEmail],
+        controller.signup
+    );
 
     router.post('/signin', controller.signin);
 };
