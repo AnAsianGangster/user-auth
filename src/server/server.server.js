cc63f56ba5a602af9d417fb261cdaccfb8105bd7
AnAsianGangster <yangaobo1996@gmail.com>
2020-12-29 14:04:01 +0800

    change file:server.js to folder:src/server/

---
+++ src/server/server.server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+require('./externalMiddleware.server')(app);
+
+
+app.get('/', function (req, res) {
+    res.json({
+        message: 'Root endpoint hit',
+    });
+});
+
+require('../routes/index.route')(app);
+
+const port = process.env.port || 5000;
+app.listen(port, function () {
+    console.log('Server is running on port', port);
+});
